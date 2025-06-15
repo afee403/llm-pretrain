1769fd006c0ad74037bcd273ae9ab87167c9adff
afee403 <afee40304@gmail.com>
2025-06-16 02:46:36 +0800

    2200013167 make embedding and unembedding matrix shared

--- train_gpt2.py
+++ train_gpt2.py
@@ -81,6 +81,8 @@ class GPT(nn.Module):
         ))
 
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+
+        self.transformer.wte.weight = self.lm_head.weight
     
     def forward(self, idx, targets=None):
         B, T = idx.size()
