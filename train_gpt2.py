b6e64eedb0121fa68f806ecf93683fb5770f9508
afee403 <afee40304@gmail.com>
2025-06-16 01:44:17 +0800

    2200013167 correct parameter and load pretrain

--- train_gpt2.py
+++ train_gpt2.py
@@ -60,11 +60,11 @@ class Block(nn.Module):
 
 @dataclass
 class GPTConfig:
-    block_size: int = 256
-    vocab_size: int = 65
-    n_layer: int = 6
-    n_head: int = 6
-    n_embd: int = 384
+    block_size: int = 1024
+    vocab_size: int = 50257
+    n_layer: int = 12
+    n_head: int = 12
+    n_embd: int = 768
 
 class GPT(nn.Module):
 
