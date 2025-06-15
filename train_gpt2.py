980ae4b84db8d7feb72cf4aabb934889ca338a1b
afee403 <afee40304@gmail.com>
2025-06-16 03:28:38 +0800

    2200013167 implement weight initialization

--- train_gpt2.py
+++ train_gpt2.py
@@ -13,6 +13,7 @@ class CausalSelfAttention(nn.Module):
         assert config.n_embd % config.n_head == 0
         self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
         self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        self.c_proj.SCALE_INIT = 1
         self.n_head = config.n_head
         self.n_embd = config.n_embd
         self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
@@ -39,6 +40,7 @@ class MLP(nn.Module):
         self.c_fc = nn.Linear(config.n_embd, 4*config.n_embd, bias=False)
         self.gelu = nn.GELU(approximate="tanh")
         self.c_proj = nn.Linear(4*config.n_embd, config.n_embd, bias=False)
+        self.c_proj.SCALE_INIT = 1
 
     def forward(self, x):
         x = self.c_fc(x)
@@ -83,6 +85,19 @@ class GPT(nn.Module):
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
 
         self.transformer.wte.weight = self.lm_head.weight
+
+        self.apply(self._init_weights)
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            std = 0.02
+            if hasattr(module, 'SCALE_INIT'):
+                std *= (2 * self.config.n_layer) ** -0.5
+            torch.nn.init.normal_(module.weight, mean=0.0, std=std)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
     
     def forward(self, idx, targets=None):
         B, T = idx.size()
@@ -209,7 +224,8 @@ import sys; sys.exit(0)
 ## generate
 
 torch.manual_seed(42)
-torch.cuda.manual_seed(42)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(42)
 while x.size(1) < max_length:
     with torch.no_grad():
         logits = model(x)
